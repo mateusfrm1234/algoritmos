ff366ac24980b2724c521594b4490d0368131882
Mateus Francisco <mateusfrancisco03@gmail.com>
2023-06-26 21:28:18 -0300

    Adicionando switch

---
+++ exemplo-switch.js
@@ -0,0 +1,13 @@
+let letra = 'a';
+
+switch (letra) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        alert('É uma vogal');
+        break;
+    default:
+        alert('Não é vogal');
+}
