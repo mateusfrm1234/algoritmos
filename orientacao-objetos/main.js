f7aa3a00490ab23bc1248a711399bb6aecabc2a7
Mateus Francisco <mateusfrancisco03@gmail.com>
2023-07-17 19:52:31 -0300

    orientacao a objetos

---
+++ orientacao-objetos/main.js
@@ -0,0 +1,9 @@
+import {Carro} from './carro.js';
+
+//instaciano o objeto up
+const up = new Carro("Up", "VW", 2015);
+
+// instaciando o objeto ka
+const ka = new Carro("Ka", "Ford", 2006);
+
+console.log(up.ano);
